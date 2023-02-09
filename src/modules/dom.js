90bc05ff78645b066ec7158610f0d7051db4eb3b
lovro <lovro.zagar5@gmail.com>
2023-02-09 16:20:06 +0000

    feat: Show loading screen on weather request/background video load, hide loading screen when background video can play

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -4,6 +4,8 @@ import weather from './weather'
 // TODO: convert hour hour to number
 const dom = (() => {
   async function loadContent() {
+    const loadingScreen = document.getElementById('loading-screen')
+    loadingScreen.classList.add('active')
     displayWeatherContent('Zagreb')
     initLocationSearch()
   }
@@ -98,12 +100,15 @@ const dom = (() => {
   function loadHourItem(hour, mainDesc, temp) {
     const hourEl = document.createElement('p')
     hourEl.textContent = hour
+    hourEl.classList.add('hour-hour')
 
     const logoEl = document.createElement('img')
     logoEl.src = mainDesc
+    logoEl.classList.add('hour-logo')
 
     const tempEl = document.createElement('p')
     tempEl.textContent = temp
+    tempEl.classList.add('hour-temp')
 
     const hourItemEl = document.createElement('div')
     hourItemEl.classList.add('hour-item')
@@ -145,21 +150,25 @@ const dom = (() => {
   function loadDayItem(day, seconds, dayIndex) {
     const weekDayEl = document.createElement('p')
     weekDayEl.textContent = secondsToWeekDay(seconds, dayIndex)
+    weekDayEl.classList.add('week-day')
 
     const logoEl = document.createElement('img')
     logoEl.src = `${day.weather[0].main.toLowerCase()}.svg`
-
-    const dayItem = document.createElement('div')
+    logoEl.classList.add('day-item-logo')
 
     const tempMinEl = document.createElement('p')
     tempMinEl.textContent = Math.round(day.temp.min)
-
-    const meterEl = document.createElement('div')
-    meterEl.classList.add('meter', 'very-cold-to-warm')
+    tempMinEl.classList.add('day-temp-min')
 
     const tempMaxEl = document.createElement('p')
     tempMaxEl.textContent = Math.round(day.temp.max)
+    tempMaxEl.classList.add('day-temp-max')
 
+    const meterEl = document.createElement('div')
+    meterEl.classList.add('meter')
+    styleMeter(meterEl, +tempMinEl.textContent, +tempMaxEl.textContent)
+
+    const dayItem = document.createElement('div')
     dayItem.classList.add('day-item')
     dayItem.appendChild(weekDayEl)
     dayItem.appendChild(logoEl)
@@ -179,12 +188,107 @@ const dom = (() => {
     return weekDay
   }
 
+  function styleMeter(meter, tempMin, tempMax) {
+    // <= 16 VERY COLD, -15 <= -1 COLD, 0 <= 14 WARM, 15 <= 29 VERY WARM, HOT >= 30
+    // VERY COLD
+    const isVeryColdToWarm =
+      tempMin < tempMax &&
+      tempMin <= -16 &&
+      (tempMin <= tempMax - 15 || tempMax >= 0)
+    const isVeryColdToCold =
+      tempMin < tempMax &&
+      tempMin <= -16 &&
+      (tempMin <= tempMax - 5 || tempMax >= -15)
+    const isVeryCold = tempMin <= -16
+    // COLD
+    const isColdToVeryWarm =
+      tempMin < tempMax &&
+      tempMin <= -1 &&
+      (tempMin <= tempMax - 15 || tempMax >= 15)
+    const isColdToWarm =
+      tempMin < tempMax &&
+      tempMin <= -1 &&
+      (tempMin <= tempMax - 5 || tempMax >= 0)
+    const isCold = tempMin <= -1
+    // WARM
+    const isWarmToHot =
+      tempMin < tempMax &&
+      tempMin <= 14 &&
+      (tempMin <= tempMax - 15 || tempMax >= 30)
+    const isWarmToVeryWarm =
+      tempMin < tempMax &&
+      tempMin <= 14 &&
+      (tempMin <= tempMax - 5 || tempMax >= 15)
+    const isWarm = tempMin <= 14
+    // VERY WARM
+    const isVeryWarmToHot =
+      tempMin < tempMax &&
+      tempMin <= 29 &&
+      (tempMin <= tempMax - 5 || tempMax >= 30)
+    const isVeryWarm = tempMin <= 29
+
+    switch (true) {
+      // VERY COLD
+      case isVeryColdToWarm:
+        meter.classList.add('very-cold-to-warm')
+        break
+      case isVeryColdToCold:
+        meter.classList.add('very-cold-to-cold')
+        break
+      case isVeryCold:
+        meter.classList.add('very-cold')
+        break
+      // COLD
+      case isColdToVeryWarm:
+        meter.classList.add('cold-to-very-warm')
+        break
+      case isColdToWarm:
+        meter.classList.add('cold-to-warm')
+        break
+      case isCold:
+        meter.classList.add('cold')
+        break
+      // WARM
+      case isWarmToHot:
+        meter.classList.add('warm-to-hot')
+        break
+      case isWarmToVeryWarm:
+        meter.classList.add('warm-to-very-warm')
+        break
+      case isWarm:
+        meter.classList.add('warm')
+        break
+      // VERY WARM
+      case isVeryWarmToHot:
+        meter.classList.add('very-warm-to-hot')
+        break
+      case isVeryWarm:
+        meter.classList.add('very-warm')
+        break
+      default:
+        meter.classList.add('hot')
+    }
+  }
+
   // BACKGROUND VIDEO
   function displayBackgroundVideo(weatherData) {
+    const loadingScreen = document.getElementById('loading-screen')
     const video = document.getElementById('background-video')
+
     let state = getWeatherState(weatherData).toLowerCase()
     if (state === 'haze') state = 'fog'
     video.src = `${state}.mp4`
+
+    // SHOW LOADING SCREEN
+    loadingScreen.classList.add('active')
+    // HIDE LOADING SCREEN WHEN VIDEO CAN PLAY
+    video.addEventListener('canplay', hideLoadingScreen)
+  }
+
+  function hideLoadingScreen() {
+    const loadingScreen = document.getElementById('loading-screen')
+    loadingScreen.removeEventListener('canplay', hideLoadingScreen)
+    loadingScreen.classList.remove('active')
   }
 
   return { loadContent }
